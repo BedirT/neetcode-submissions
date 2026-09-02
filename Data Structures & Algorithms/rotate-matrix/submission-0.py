17489c7b3f954b6099f6b314d0f05926603f3178
Bedir Tapkan <tapkan@ualberta.ca>
2026-09-02 11:24:04 -0700

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        rotated = {}
+        for r in range(n):
+            for c in range(n):
+                if (r, c) in rotated:
+                    val = rotated[(r, c)]
+                else:
+                    val = matrix[r][c]
+                new_loc = c, (n - r - 1) # new r, c
+                rotated[new_loc] = matrix[new_loc[0]][new_loc[1]]
+                matrix[new_loc[0]][new_loc[1]] = val
+                print((r, c), "->", new_loc)
